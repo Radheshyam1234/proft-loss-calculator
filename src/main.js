fe6b6f9a5b7ac14779fcd0f11af1e4fca67b9a97
Radheshyam1234 <88738458+Radheshyam1234@users.noreply.github.com>
2021-08-22 15:12:51 +0530

    Fix bugs

--- src/main.js
+++ src/main.js
@@ -3,54 +3,65 @@ var stocks = document.querySelector("#stocks-quantity");
 var current = document.querySelector("#current-price");
 var calculateBtn = document.querySelector("#calc-btn");
 var outputBox = document.querySelector("#output-box");
-var output= document.querySelector(".output")
+var body=document.querySelector("body")
+//var output= document.querySelector(".output")
 
-calculateBtn.addEventListener("click",function (){
-    var initialPrice=initial.value;
-    var quantity=stocks.value;
-    var currentPrice=current.value
-    output.style.display="block";
+calculateBtn.addEventListener("click", ()=>{
+    const initialPrice=Number(initial.value);
+    const quantity=Number(stocks.value);
+    const currentPrice=Number(current.value)
+    outputBox.style.display="block";
     findProfitOrLoss(initialPrice,quantity,currentPrice)
 });
 
 
 
 function findProfitOrLoss(initialPrice,quantity,currentPrice)  {
-  if(!initialPrice||!quantity||!currentPrice){
+
+ if(!initialPrice||!quantity||!currentPrice){
       showOutput("👿👿👿Please fill all the details👆👆👆👆")
-      output.style["background-color"] = " rgb(0, 38, 255)";
+      outputBox.style["background-color"] = " rgb(0, 38, 255)";
+      body.style["background-color"]="#f1c40f"
   }
- else{
-     if (initialPrice > currentPrice){
+  
 
-            var loss=quantity*(initialPrice-currentPrice);
-            var lossPercent=(loss/(initialPrice))*100;
+
+ else if (initialPrice > currentPrice){
+   
+    var loss=quantity*(initialPrice-currentPrice);
+                var lossPercent=(loss/(initialPrice*quantity))*100;
+                
+                outputBox.style["background-color"] = "red";
+    
+              showOutput(`The loss amount is ${loss} and the loss percentage is ${lossPercent}% 😫😫😫😫`);
+              body.style["background-color"]="#ff3f34"
+              
+
+}
+
+else if (currentPrice > initialPrice){
+   
+
+    var profit= quantity *(currentPrice-initialPrice)
+            var profitPercent=(profit/(initialPrice*quantity))*100;
             
-            output.style["background-color"] = "red";
-
-          showOutput(`The loss amount is ${loss} and the loss percentage is ${lossPercent}% 😫😫😫😫`);
-            }
-
-            else if (currentPrice>initialPrice){
-
-            var profit= quantity *(currentPrice-initialPrice)
-            var profitPercent=(profit/(initialPrice))*100;
-            
-             output.style["background-color"] = "green";
+             outputBox.style["background-color"] = "green";
             showOutput(`😲😲😲😲The Profit amount is ${profit} and the profit percentage is ${profitPercent}%`);
-           
-            }
 
-            
-        else{
-            outputBox.innerHTML="No profit No loss..Don't worry Be happy !!!";
-            output.style["background-color"] = "purple";
-        }
-    }
+            body.style["background-color"]="#27ae60"
+
+}
+else {
+    outputBox.innerHTML="No profit No loss..Don't worry Be happy !!!";
+          outputBox.style["background-color"] = "purple";
+          body.style["background-color"]="#ecf0f1"
+}
+
+
+ 
 }
 
 function showOutput(message){
     outputBox.innerHTML=message;
 }
-
 
